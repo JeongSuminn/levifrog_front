9897ad6b9a6fdb56063d8e053ff4c5dc8645855b
JeongSuminn <susuhome2@gmail.com>
2024-05-31 18:37:08 +0900

    static page style 수정

--- js/model.js
+++ js/model.js
@@ -11,12 +11,13 @@ let renderer = new THREE.WebGLRenderer({
 
 renderer.outputEncoding = THREE.sRGBEncoding;
 let camera = new THREE.PerspectiveCamera(30, 1);
-camera.position.set(-1.9, 0, 2.5);  // 카메라 위치 설정
+camera.position.set(-1.7, 0, 2.5);  // 카메라 위치 설정
 
 // OrbitControls를 추가합니다.
 let controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true; // 부드러운 회전을 위한 damping 활성화
 controls.dampingFactor = 0.25;
+controls.enableZoom = false;
 
 let loader = new GLTFLoader();
 loader.load('model/model_levifrog.glb', function (gltf) {
